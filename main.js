02fc92e2eff8f0d8fc35e7b4b449c1a4cefc71e4
RabbitMimi <kitto.ringo@gmail.com>
2024-11-02 21:51:56 +0900

    js実装

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 "use strict";
+{
+  const year = 2000;
+  const month = 4; //5月
+
+  function getCalenderBody() {
+    const dates = [];
+    const lastDate = new Date(year, month + 1, 0).getDate();
+
+    for (let i = 1; i <= lastDate; i++) {
+      dates.push(i);
+    }
+
+    console.log(dates);
+  }
+
+  getCalenderBody();
+}
